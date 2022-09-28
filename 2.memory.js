8f16c1170ac956925e1c600864ac7d4767ced07f
윤종경 <jkyoon@danawa.com>
2022-09-28 09:40:48 +0900

    function memory

--- 2.memory.js
+++ 2.memory.js
@@ -1,7 +1,13 @@
-function add (a, b){
-  return a + b
+function add(a, b) {
+  console.log(a);
+  // undefined
+  console.log(b);
+  // undefined
+  return a + b;
 }
 
-const sum = add(1,2);
+const sum = add(1, 2);
 
-console.log(sum)
+console.log(sum);
+console.log(add())
+// NaN
