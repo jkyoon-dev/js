bedf303f8b0c87aff657edaee0769b7a73fdcf24
윤종경 <jkyoon@danawa.com>
2022-09-30 13:45:30 +0900

    퀴즈 해설

--- quiz2.js
+++ quiz2.js
@@ -1,22 +1,53 @@
-class Emploree {
-  #time;
-  constructor (name, partName, time) {
+class Employee {
+  constructor(name, department, hoursPerMonth, payRate) {
     this.name = name;
-    this.partName = partName;
-    if(isNaN(time) || time < 0){
-      this.#time = 0
-    } else{
-      this.#time = time;
-    }
+    this.department = department;
+    this.hoursPerMonth = hoursPerMonth;
+    this.payRate = payRate;
   }
-  get partTimeJob(){
-    console.log(`${this.time * 160 * 8000}원입니다.`)
-  }
-  get fullTimeJob(){
-    console.log(`${this.time * 160 * 10000}원입니다.`)
-
+  calculatePay() {
+    return this.hoursPerMonth * this.payRate;
   }
 }
 
-const JK = new Emploree ('JK', 'alba', 10);
-JK.partTimeJob()
+class FullTimeEmployee extends Employee {
+  static PAY_RATE = 10000;
+  constructor(name, department, hoursPerMonth) {
+    super(name, department, hoursPerMonth, FullTimeEmployee.PAY_RATE);
+  }
+}
+
+class PartTImeEmployee extends Employee {
+  static PAY_RATE = 8000;
+  constructor(name, department, hoursPerMonth){
+    super(name, department, hoursPerMonth, PartTImeEmployee.PAY_RATE)
+  }
+}
+
+const ellie = new FullTimeEmployee("엘리", "s/w", 30);
+const bob = new PartTImeEmployee("밥", "s/w", 20);
+console.log(ellie.calculatePay());
+console.log(bob.calculatePay());
+
+// class Emploree {
+//   #time;
+//   constructor (name, partName, time) {
+//     this.name = name;
+//     this.partName = partName;
+//     if(isNaN(time) || time < 0){
+//       this.#time = 0
+//     } else{
+//       this.#time = time;
+//     }
+//   }
+//   get partTimeJob(){
+//     console.log(`${this.time * 160 * 8000}원입니다.`)
+//   }
+//   get fullTimeJob(){
+//     console.log(`${this.time * 160 * 10000}원입니다.`)
+
+//   }
+// }
+
+// const JK = new Emploree ('JK', 'alba', 10);
+// JK.partTimeJob()
